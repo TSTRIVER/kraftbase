13b90556792b333e5a1fee59bea46a90d9c6f1ad
Muhammad Taaha <96954628+TSTRIVER@users.noreply.github.com>
2023-12-27 02:11:10 +0530

    refactored

--- src/app/util.js
+++ src/app/util.js
@@ -1,6 +1,6 @@
 export const fetchListings = async () => {
   let res = await fetch(
-    "https://kraftbase-7e98-2q7lmcp1s-tstriver.vercel.app/api/listings",
+    "https://kraftbase-7e98-tstriver.vercel.app/api/listings",
     { next: { revalidate: 1 } }
   );
   let data = res.json();
