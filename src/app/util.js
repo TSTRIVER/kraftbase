046bc9cab4e33eea4d9f6ba9642483fc129e805c
Muhammad Taaha <96954628+TSTRIVER@users.noreply.github.com>
2023-12-27 02:08:02 +0530

    discharge

--- src/app/util.js
+++ src/app/util.js
@@ -1,5 +1,8 @@
-export const fetchListings = async() => {
-    let res = await fetch("http://localhost:3000/api/listings",{ next: {revalidate: 1}});
-    let data = res.json();
-    return data;
-}
+export const fetchListings = async () => {
+  let res = await fetch(
+    "https://kraftbase-7e98-2q7lmcp1s-tstriver.vercel.app/api/listings",
+    { next: { revalidate: 1 } }
+  );
+  let data = res.json();
+  return data;
+};
